29a49ae476664b957b0aefe3ec86ea1ba3bc4bd7
Adam Beguelin <adam@beguelin.com>
2017-03-17 22:58:11 -0400

    pull out the speed, temp, and gust

--- index.js
+++ index.js
@@ -3,16 +3,24 @@ var request = require('request');
 
 exports.helloWorld = (req, res) => res.send("Cheerio, World!");
 
-exports.get = function get (req, res) {
+exports.hello = function (req, res) {
 
 	url = 'http://www.weatherlink.com/user/cokikite/index.php?view=main&headers=0'
     request(url, function(error, response, html){
 		
 		if(!error){ 
 			// log something
-			console.log(html);
+			var $ = cheerio.load(html);
+			var speed = $("td.glamor_datatemp:contains('KT')").text(); 
+			var temp = $("td.glamor_temp").text().match(/\d+/); 			
+			var gust = $("td.glamor_detailtemp:contains('KT')").text().match(/\d+/);
+
+			res.send(speed.substring(0,speed.length-3) + '\n' + gust + '\n'  + temp);	
+
+		} else {
+			console.log(error);
+			res.send("error")
 		}
 	})
-	res.send("yo get got");	
 
 }
