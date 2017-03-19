03152693eb443cac79e81062a6cdc9cdca0ccee5
Adam Beguelin <adam@beguelin.com>
2017-03-19 06:48:04 -0400

    assert the direction is only NEWS

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 var cheerio = require('cheerio');
 var request = require('request');
+var assert = require('assert');
 
 exports.helloWorld = (req, res) => res.send("Cheerio, World!");
 
+// set 
+// function set(record) {
+// const entity = {
+//   key: key,
+//   data: req.body.value
+// };
+//
+//
+//
+// return datastore.save(entity)
+//   .then(() => res.status(200).send(`Entity ${key.path.join('/')} saved.`))
+//   .catch((err) => {
+//     console.error(err);
+//     res.status(500).send(err);
+//     return Promise.reject(err);
+//   });
+//
+// }
+
+  
 exports.hello = function (req, res) {
 
 	url = 'http://www.weatherlink.com/user/cokikite/index.php?view=main&headers=0'
@@ -16,8 +37,10 @@ exports.hello = function (req, res) {
 			var speed = dir_speed[1]
 			var temp = $("td.glamor_temp").text().match(/\d+/); 			
 			var gust = $("td.glamor_detailtemp:contains('KT')").text().match(/\d+/);
+			var now = new Date();
 
-			res.send(dir + '\n' + speed + '\n' + gust + '\n'  + temp);	
+			assert(dir.match(/[NEWS]+/),"direction has only NEWS characters");
+			res.send(dir + '\n' + speed + '\n' + gust + '\n'  + temp + '\n' + now);	
 
 		} else {
 			console.log(error);
