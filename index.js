45179002f5bacffd3ac9ac15c35372708a7e8fb5
Ricardo Nazario <nazarioricardo@gmail.com>
2017-04-15 13:32:15 -0400

    Uploading to datastore, and getting readings back

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var cheerio = require('cheerio');
 var request = require('request');
+var fs = require('fs')
+var plotly = require('plotly')("nazarioricardo", "nvJBWlrvBwapuT9PfCC0")
 var assert = require('assert');
 
 const Datastore = require('@google-cloud/datastore');
 
 // Your Google Cloud Platform project ID
-const projectId = 'gcf-koki';
+const projectId = 'cheers-164215';
 
 // Instantiates a client
 const datastore = Datastore({
   projectId: projectId
 });
-
 
 exports.helloWorld = (req, res) => res.send("Cheerio, World!");
 
@@ -34,34 +35,49 @@ exports.helloWorld = (req, res) => res.send("Cheerio, World!");
 //
 // }
 
-  
+
 exports.hello = function (req, res) {
 
 	url = 'http://www.weatherlink.com/user/cokikite/index.php?view=main&headers=0'
-    request(url, function(error, response, html){
+    
+		request(url, function(error, response, html){
 		
-		if(!error){ 
+		if(!error) {
+
 			// log something
 			var $ = cheerio.load(html);
 			var dir_speed = $("td.glamor_datatemp:contains('KT')").text().split(/\s/); 
-			var dir = dir_speed[0];
+			var dir = dir_speed[0]
+			// Sometimes speed is simply 'Calm'
 			var speed = dir_speed[1]
 			var temp = $("td.glamor_temp").text().match(/\d+/)[0]; 			
 			var gust = $("td.glamor_detailtemp:contains('KT')").text().match(/\d+/)[0];
-			var now = new Date();
+			var now = new Date()
+			var date = now.toDateString()
+			var time = now.toLocaleTimeString()
+
+			/*
+			
+			TODO: Get date and time values from Weatherlink with REGEX in order to have accurate time for Ocean Park
+
+			var date = $("td.glamor_timestamp>div").text().slice(39, 38)
+			var time = $("td.glamor_timestamp>div").text().slice(31, 38)
+			
+			*/
 
 			// sanity check the values
-			if(!dir.match(/^[NEWS]+$/)){console.log("Wind direction seems wrong: "+ dir);}
-			if(!speed.match(/^\d+$/)){console.log("Wind speed seems wrong: '"+ speed + "'");}
+			// if(!dir.match(/^[NEWS]+$/)) { console.log("Wind direction seems wrong: " + dir) }
+			// if(!speed.match(/^\d+$/)) { console.log("Wind speed seems wrong: " + speed) }
 			// these checks fail, maybe because temp is a object not a string?
-			// if(!temp.match(/^\d+$/)){console.log("Wind temp seems wrong: '"+ temp + "'");}
-			// if(!gust.match(/^\d+$/)){console.log("Wind temp seems gust: '"+ gust + "'");}
+			// if(!temp.match(/^\d+$/)) { console.log("Wind temp seems wrong: '"+ temp + "'") }
+			// if(!gust.match(/^\d+$/)) { console.log("Wind temp seems gust: '"+ gust + "'") }
 			
 
 			// The kind for the new entity
 			const kind = 'Wind-reading';
 			// The name/ID for the new entity
 			const name = now;
+
 			// The Cloud Datastore key for the new entity
 			const taskKey = datastore.key([kind, name]);
 
@@ -69,10 +85,12 @@ exports.hello = function (req, res) {
 			const reading = {
 			  key: taskKey,
 			  data: {
-			    direction: dir,
+				  direction: dir,
 				  speed: speed,
 				  temp: temp,
-				  gust: gust
+				  gust: gust,
+				  date: date,
+				  time: time
 			  }
 			};
 
@@ -91,5 +109,33 @@ exports.hello = function (req, res) {
 			res.send("error")
 		}
 	})
+}
 
+exports.getReadings = function (req, res) {
+
+	const date = new Date()
+	const kind = 'Wind-reading'
+	
+	let todayString = date.toDateString()
+
+	const query = datastore.createQuery(kind)
+		.filter('date', '=', todayString)
+	
+	return datastore.runQuery(query)
+		.then((results) => {
+			const readings = results[0]
+				.sort(function(a,b) {
+					return a.time < b.time ? 1 : ((b.last_nom < a.last_nom) ? -1 : 0)
+				})
+
+				console.log("added" + add(1,2))
+			
+			console.log('Wind Readings: ')
+			readings.forEach((reading) => {
+				const readingKey = reading[datastore.KEY]
+				console.log(readingKey.id, reading)
+			})
+			console.log("readings" + readings)
+			return res.status(200).send(JSON.stringify(readings))
+		})
 }
